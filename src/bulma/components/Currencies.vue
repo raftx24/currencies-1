8c5eab02f2a8cc2b847d225f1631be6e4c6b8863
mishu <mishu@xtelecom.ro>
2019-11-12 12:20:18 +0200

    adds boolean symbol

--- src/bulma/components/Currencies.vue
+++ src/bulma/components/Currencies.vue
@@ -1,53 +1,14 @@
 <template>
-    <a class="button is-warning"
-        @click="next">
-        <span class="is-bold">
-            {{ currency.symbol }}
-        </span>
-    </a>
-</template>
-
-<script>
-import { mapState } from 'vuex';
-
-export default {
-    name: 'Currencies',
-
-    props: {
-        value: {
-            type: Number,
-            default: null,
-        },
-        readonly: {
-            type: Boolean,
-            default: false,
-        },
-    },
-
-    computed: {
-        ...mapState(['enums']),
-        ...mapState('local', ['currencies']),
-        default() {
-            return this.currencies.find(({ isDefault }) => isDefault);
-        },
-        currency() {
-            return this.currencies.find(({ id }) => id === this.value);
-        },
-        index() {
-            return this.currencies.findIndex(({ id }) => id === this.value);
-        },
-    },
-
-    created() {
-        if (!this.value) {
-            this.$emit('input', this.default.id);
-        }
-    },<template>
-    <a class="button is-warning"
+    <a class="button"
         v-tooltip="tooltip ? currency.shortName : null"
         @click="next">
-        <span class="is-bold">
+        <span class="is-bold"
+            v-if="symbol">
             {{ currency.symbol }}
+        </span>
+        <span class="is-bold"
+            v-else>
+            {{ currency.shortName }}
         </span>
     </a>
 </template>
@@ -67,6 +28,10 @@ export default {
             default: null,
         },
         readonly: {
+            type: Boolean,
+            default: false,
+        },
+        symbol: {
             type: Boolean,
             default: false,
         },
@@ -95,23 +60,6 @@ export default {
             this.$emit('input', this.default.id);
         }
     },
-
-    methods: {
-        next() {
-            if (this.readonly) {
-                return;
-            }
-
-            const index = this.index === this.currencies.length - 1
-                ? 0
-                : this.index + 1;
-
-            this.$emit('input', this.currencies[index].id);
-        },
-    },
-};
-</script>
-
 
     methods: {
         next() {
